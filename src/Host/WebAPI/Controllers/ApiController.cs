3b1af9fbff976034e480f0c627a3bf05fdbb5b08
Raphael Ramirez <ralph@s2dioapps.com>
2022-11-21 18:22:50 +0800

    [S2DIO-156] - added error endpoint for global error handling

---
+++ src/Host/WebAPI/Controllers/ApiController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Amina.WebAPI.Controllers;
+
+public class ApiController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ src/Host/WebAPI/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Amina.WebAPI.Common.Errors;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+
+namespace Amina.WebAPI;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddPresentation(this IServiceCollection services)
+    {
+        services.AddControllers();
+
+        services.AddSingleton<ProblemDetailsFactory, AminaProblemDetailsFactory>();
+
+        return services;
+    }
+}
