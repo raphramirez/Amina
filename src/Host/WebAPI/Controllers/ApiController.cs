c15777f08ab54ac777ba426be8e2beb1007034c5
Raphael Ramirez <ralph@s2dioapps.com>
2022-11-22 10:35:44 +0800

    [S2DIO-156] - added api controller with validation behavior

--- src/Host/WebAPI/Controllers/ApiController.cs
+++ src/Host/WebAPI/Controllers/ApiController.cs
@@ -1,11 +1,52 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Amina.WebAPI.Common.Http;
+using ErrorOr;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace Amina.WebAPI.Controllers;
 
-public class ApiController : Controller
+public class ApiController : ControllerBase
 {
-    public IActionResult Index()
+    protected IActionResult Problem(List<Error> errors)
     {
-        return View();
+        if (errors.Count is 0)
+        {
+            return Problem();
+        }
+
+        if (errors.All(error => error.Type == ErrorType.Validation))
+        {
+            return ValidationProblem(errors);
+        }
+
+        HttpContext.Items[HttpContextItemKeys.Errors] = errors;
+        return Problem(errors[0]);
+    }
+
+    private IActionResult Problem(Error error)
+    {
+        var statusCode = error.Type switch
+        {
+            ErrorType.Conflict => StatusCodes.Status409Conflict,
+            ErrorType.Validation => StatusCodes.Status400BadRequest,
+            ErrorType.NotFound => StatusCodes.Status404NotFound,
+            _ => StatusCodes.Status500InternalServerError
+        };
+
+        return Problem(statusCode: statusCode, title: error.Description);
+    }
+
+    private IActionResult ValidationProblem(List<Error> errors)
+    {
+        var modelStateDictionary = new ModelStateDictionary();
+
+        foreach (var error in errors)
+        {
+            modelStateDictionary.AddModelError(
+                error.Code,
+                error.Description);
+        }
+
+        return ValidationProblem(modelStateDictionary);
     }
 }
